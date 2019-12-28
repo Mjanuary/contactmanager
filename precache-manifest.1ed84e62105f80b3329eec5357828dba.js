e0fc09fb210472167b60f81410252e2611625cac
Mjanuary <janviermuhawenimana@gmail.com>
2019-12-28 23:46:43 +0200

    Updates

---
+++ precache-manifest.1ed84e62105f80b3329eec5357828dba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8b453e33a13a14448faf01d9e33efd",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "0370cb0f415d3a7055d8",
+    "url": "/contactmanager/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "701defddaac399074348",
+    "url": "/contactmanager/static/css/main.c2bbfd91.chunk.css"
+  },
+  {
+    "revision": "0370cb0f415d3a7055d8",
+    "url": "/contactmanager/static/js/2.814fded1.chunk.js"
+  },
+  {
+    "revision": "c8336e0bcbf8ea1670aafb15aa044bf7",
+    "url": "/contactmanager/static/js/2.814fded1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "701defddaac399074348",
+    "url": "/contactmanager/static/js/main.0bfb7d26.chunk.js"
+  },
+  {
+    "revision": "c991f574bb8ff25e151d",
+    "url": "/contactmanager/static/js/runtime-main.547f0f82.js"
+  }
+]);
